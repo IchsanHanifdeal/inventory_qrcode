e6cd0523ab5c4f47e1e29e4463d6760f27338e13
zaadevofc <zaadevofc@gmail.com>
2024-05-28 00:06:42 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,27 +1,40 @@
-<x-main title="Login" class="p-0">
-    <div
-        class="bg-no-repeat bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')]">
-        <div class="flex justify-end">
-            <div class="bg-white min-h-screen w-1/2 flex justify-center items-center">
-                <div>
-                    <form>
-                        <div class="my-3">
-                            <label class="block text-md mb-2" for="email">Email</label>
-                            <input class="input input-bordered px-4 w-full border-2 py-2 rounded-md text-sm outline-none"
-                                type="email" name="email" placeholder="Masukan email...">
-                        </div>
-                        <div class="my-3">
-                            <label class="block text-md mb-2" for="email">Password</label>
-                            <input class="input input-bordered px-4 w-full border-2 py-2 rounded-md text-sm outline-none"
-                                type="password" name="password" placeholder="Masukan password...">
-                        </div>
-                        <div class="">
-                            <button class="mt-4 mb-3 w-full bg-blue-500 hover:bg-blue-400 text-white py-2 rounded-md">
-                                Login
-                            </button>
-                        </div>
-                    </form>
-                </div>
+<x-main title="Login" class="p-0" full>
+    <section class="min-h-screen flex items-stretch text-white ">
+        <div
+            class="lg:flex w-1/2 hidden bg-gray-500 bg-no-repeat bg-cover relative items-center bg-[url('https://www.hashmicro.com/id/blog/wp-content/uploads/2022/10/image-25-1.jpg')]">
+            <div class="absolute bg-[linear-gradient(180deg,transparent,rgba(0,0,0,1))] inset-0 z-0"></div>
+            <div class="w-full px-24 z-10">
+                <p class="text-4xl leading-tight tracking-wide font-semibold max-w-lg">Solusi kebutuhan barang untuk
+                    operasi perusahaan.</p>
+            </div>
+            <div class="bottom-0 absolute p-4 text-center right-0 left-0 flex justify-center space-x-4">
+                <span>
+                    @include('components.brands')
+                </span>
+            </div>
+        </div>
+        <div class="lg:w-1/2 bg-[#161616] w-full flex items-center justify-center text-center md:px-16 px-0 z-0">
+            <div
+                class="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center bg-[url('https://www.hashmicro.com/id/blog/wp-content/uploads/2022/10/image-25-1.jpg')]">
+                <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
+            </div>
+            <div class="w-full py-6 z-20">
+                <h1 class="my-6">
+                    @include('components.brands', ['class' => 'text-3xl'])
+                </h1>
+                <form class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
+                    <div class="pb-2 pt-4">
+                        <input type="email" name="email" id="email" placeholder="Masukan email..."
+                            class="input block w-full p-4 text-lg bg-black">
+                    </div>
+                    <div class="pb-2 pt-4">
+                        <input class="input block w-full p-4 text-lg bg-black" type="password" name="password"
+                            id="password" placeholder="Masukan password...">
+                    </div>
+                    <div class="pb-2 pt-4">
+                        <button class="btn w-full">Masuk</button>
+                    </div>
+                </form>
             </div>
         </div>
     </section>
